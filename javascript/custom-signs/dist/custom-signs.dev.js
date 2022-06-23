2801d1cfe4f39359019921324d6a54dc00b26120
Cristian David Casallas Velandia <cricadev@Cristians-MacBook-Pro.local>
2022-06-23 16:08:28 -0500

    learned about ternary operators and template strings with custom signs

--- javascript/custom-signs/dist/custom-signs.dev.js
+++ javascript/custom-signs/dist/custom-signs.dev.js
@@ -1,5 +1,14 @@
-// @ts-check
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.buildSign = buildSign;
+exports.buildBirthdaySign = buildBirthdaySign;
+exports.graduationFor = graduationFor;
+exports.costOf = costOf;
+
+// @ts-check
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
@@ -12,11 +21,9 @@
  *
  * @returns {string} template string combining both parameters
  */
-
-export function buildSign(occasion, name) {
-  return `Happy ${occasion} ${name}!`
+function buildSign(occasion, name) {
+  return "Happy ".concat(occasion, " ").concat(name, "!");
 }
-
 /**
  * Build a birthday sign that conditionally formats the return string.
  *
@@ -25,10 +32,10 @@ export function buildSign(occasion, name) {
  * @returns {string} template string based on age
  */
 
-export function buildBirthdaySign(age) {
-  return `${age >= 50 ?  "Happy Birthday! What a mature fellow you are.": "Happy Birthday! What a young fellow you are."}`
-}
 
+function buildBirthdaySign(age) {
+  return "".concat(age >= 50 ? "Happy Birthday! What a mature fellow you are." : "Happy Birthday! What a young fellow you are.");
+}
 /**
  * Build a graduation sign that includes multiple lines.
  *
@@ -38,10 +45,10 @@ export function buildBirthdaySign(age) {
  * @returns {string} multi-line template string
  */
 
-export function graduationFor(name, year) {
-  return `Congratulations ${name}!\nClass of ${year}`
-}
 
+function graduationFor(name, year) {
+  return "Congratulations ".concat(name, "!\nClass of ").concat(year);
+}
 /**
  * Determine cost based on each character of sign parameter that builds
  * the template string that includes the currency parameter.
@@ -52,7 +59,7 @@ export function graduationFor(name, year) {
  * @returns {string} cost to create the sign
  */
 
-export function costOf(sign, currency) {
-  return `Your sign costs ${20+(sign.length*2)}.00 ${currency}.`;
 
+function costOf(sign, currency) {
+  return "Your sign costs ".concat(20 + sign.length * 2, ".00 ").concat(currency, ".");
 }
