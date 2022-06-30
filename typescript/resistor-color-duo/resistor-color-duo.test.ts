1cefc9f943ecbf96b0b3a7914540beb7ce91bc61
Cristian David Casallas Velandia <cricadev@Cristians-MacBook-Pro.local>
2022-06-29 21:22:51 -0500

    complete resistor color duo

--- typescript/resistor-color-duo/resistor-color-duo.test.ts
+++ typescript/resistor-color-duo/resistor-color-duo.test.ts
@@ -1,23 +1,23 @@
-import { decodedValue } from './resistor-color-duo'
+import { decodedValue } from "./resistor-color-duo";
 
-describe('Resistor Colors', () => {
-  it('Brown and black', () => {
-    expect(decodedValue(['brown', 'black'])).toEqual(10)
-  })
+describe("Resistor Colors", () => {
+  it("Brown and black", () => {
+    expect(decodedValue(["brown", "black"])).toEqual(10);
+  });
 
-  xit('Blue and grey', () => {
-    expect(decodedValue(['blue', 'grey'])).toEqual(68)
-  })
+  it("Blue and grey", () => {
+    expect(decodedValue(["blue", "grey"])).toEqual(68);
+  });
 
-  xit('Yellow and violet', () => {
-    expect(decodedValue(['yellow', 'violet'])).toEqual(47)
-  })
+  it("Yellow and violet", () => {
+    expect(decodedValue(["yellow", "violet"])).toEqual(47);
+  });
 
-  xit('Orange and orange', () => {
-    expect(decodedValue(['orange', 'orange'])).toEqual(33)
-  })
+  it("Orange and orange", () => {
+    expect(decodedValue(["orange", "orange"])).toEqual(33);
+  });
 
-  xit('Ignore additional colors', () => {
-    expect(decodedValue(['green', 'brown', 'orange'])).toEqual(51)
-  })
-})
+  it("Ignore additional colors", () => {
+    expect(decodedValue(["green", "brown", "orange"])).toEqual(51);
+  });
+});
--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,17 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+//@ts-check
+export enum ResistorValues {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+}
+type Color = keyof typeof ResistorValues;
+
+export function decodedValue([first, second]: Color[]): number {
+  return Number(`${ResistorValues[first]}${ResistorValues[second]}`);
 }
