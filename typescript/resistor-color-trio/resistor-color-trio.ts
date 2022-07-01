af4f3bfb5d8803808a6a822e33a61fb77f8f6d39
Cristian David Casallas Velandia <cricadev@Cristians-MacBook-Pro.local>
2022-06-30 20:59:21 -0500

    analuzing how to solve chall

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,19 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export enum ResistorValues {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
 }
+type Color = keyof typeof ResistorValues;
+
+export function decodedResistorValue([first, second, third]: Color[]): number {
+  return Number(
+    `${ResistorValues[first]}${ResistorValues[second]}${ResistorValues[third]}`
+  );
+}
+decodedResistorValue(["orange", "orange", "orange"]);
