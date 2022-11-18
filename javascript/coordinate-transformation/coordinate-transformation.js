93da558bd6ab401f88a724bbc8f906b7560a6399
CricaDev <cristiancasallas998@gmail.com>
2022-11-18 09:56:00 -0500

    bunch of exercises resolved

--- javascript/coordinate-transformation/coordinate-transformation.js
+++ javascript/coordinate-transformation/coordinate-transformation.js
@@ -1,6 +1,10 @@
 // @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
 /**
- * Create a function that returns a function making use of a closure to
+ * Create a function which returns a function making use of a closure to
  * perform a repeatable 2d translation of a coordinate pair.
  *
  * @param {number} dx the translate x component
@@ -9,14 +13,13 @@
  * @returns {function} a function which takes an x, y parameter, returns the
  *  translated coordinate pair in the form [x, y]
  */
-export function translate2d(dx, dy) {
-  return function move(x,y){
-    return [dx+=x, dy+y]
+ export function translate2d(dx, dy) {
+  return function translate(x, y) {
+    return [x + dx, y + dy];
   }
 }
-
 /**
- * Create a function that returns a function making use of a closure to
+ * Create a function which returns a function making use of a closure to
  * perform a repeatable 2d scale of a coordinate pair.
  *
  * @param {number} sx the amount to scale the x component
@@ -26,45 +29,43 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  return function move(x,y){
-    return [sx*x, sy*y]
+  return function scale(x, y) {
+    return [x * sx, y * sy];
   }
-  
 }
-
 /**
- * Create a composition function that returns a function that combines two
+ * Create a composition function which returns a function that combines two
  * functions to perform a repeatable transformation
  *
  * @param {function} f the first function to apply
  * @param {function} g the second function to apply
- *    
+ *
  * @returns {function} a function which takes an x, y parameter, returns the
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  return function(x, y) {
-    return g(...f(x, y));
+  return function composed(x, y) {
+    return g(...f(x, y))
   }
 }
-
 /**
- * Return a function that memoizes the last result.  If the arguments are the same as the last call,
+ * Return a function which memoizes the last result.  If the arguments are the same as the last call,
  * then memoized result returned.
  *
  * @param {function} f the transformation function to memoize, assumes takes two arguments 'x' and 'y'
  *
- * @returns {function} a function which takes x and y arguments, and will either return the saved result
+ * @returns {function} a function which takes and x, y argument, and will either return the saved result
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  let prevX, prevY;
-  let prevVal
-  return function(x, y) {
-    if (x == prevX && y == prevY) {
-      return prevVal;
-    } else {
-      return prevVal = f(prevX = x, prevY = y);
+  let previousX, previousY, previousResult
+  
+  return function memoised(x, y) {
+    if (previousX === x && previousY === y) {
+      return previousResult
     }
+    previousX = x
+    previousY = y
+    return previousResult = f(x, y)
   }
 }
